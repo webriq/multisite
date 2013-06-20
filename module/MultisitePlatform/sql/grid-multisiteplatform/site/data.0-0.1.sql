dd60c6f482ca3f0508d6499ab9ffbc1bd2e25268
David Pozsar <david.pozsar@megaweb.hu>
2013-06-20 13:58:21 +0200

    add some sql--patches

---
+++ module/MultisitePlatform/sql/grid-multisiteplatform/site/data.0-0.1.sql
@@ -0,0 +1,21 @@
+-- insert default values for table: user_right
+
+DO LANGUAGE plpgsql $$
+BEGIN
+
+    IF NOT EXISTS ( SELECT *
+                      FROM "user_right"
+                     WHERE "group"      = 'user.extra'
+                       AND "resource"   = 'admin'
+                       AND "privilege"  = 'ui' ) THEN
+
+        INSERT INTO "user_right" ( "label", "group", "resource", "privilege", "optional" )
+             VALUES ( NULL, 'user.extra', 'admin', 'ui', FALSE );
+
+    END IF;
+
+END $$;
+
+INSERT INTO "user_right" ( "label", "group", "resource", "privilege", "optional", "module" )
+     VALUES ( NULL, 'user.extra', 'central.site', 'view', TRUE, 'Grid\MultisiteCentral' ),
+            ( NULL, 'central.site', 'central.site', 'create', TRUE, 'Grid\MultisiteCentral' );
