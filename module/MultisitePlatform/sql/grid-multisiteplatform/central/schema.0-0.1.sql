f5ed34351db9711f3553817b926a3b871d3db6f3
David Pozsar <david.pozsar@megaweb.hu>
2013-09-24 13:28:55 +0200

    formatting

--- module/MultisitePlatform/sql/grid-multisiteplatform/central/schema.0-0.1.sql
+++ module/MultisitePlatform/sql/grid-multisiteplatform/central/schema.0-0.1.sql
@@ -153,21 +153,21 @@ CREATE TABLE "_central"."domain"
 --------------------------------------------------------------------------------
 
 CREATE OR REPLACE VIEW "_central"."fulldomain" AS
-SELECT "site"."id"      AS "siteId",
-       "site"."schema",
-       "site"."ownerId",
-       "site"."created",
-       "domain"."id"    AS "domainId",
-       "domain"."domain",
-       "subdomain"."id" AS "subdomainId",
-       "subdomain"."subdomain",
-       btrim( COALESCE( "subdomain"."subdomain", '' )
-           || '.' || "domain"."domain", '.' ) AS "fulldomain"
-  FROM "_central"."site"
-  JOIN "_central"."domain"
-    ON "domain"."siteId"    = "site"."id"
-  JOIN "_central"."subdomain"
-    ON "subdomain"."siteId" = "site"."id";
+           SELECT "site"."id"      AS "siteId",
+                  "site"."schema",
+                  "site"."ownerId",
+                  "site"."created",
+                  "domain"."id"    AS "domainId",
+                  "domain"."domain",
+                  "subdomain"."id" AS "subdomainId",
+                  "subdomain"."subdomain",
+                  btrim( COALESCE( "subdomain"."subdomain", '' )
+                      || '.' || "domain"."domain", '.' ) AS "fulldomain"
+             FROM "_central"."site"
+             JOIN "_central"."domain"
+               ON "domain"."siteId"    = "site"."id"
+             JOIN "_central"."subdomain"
+               ON "subdomain"."siteId" = "site"."id";
 
 --------------------------------------------------------------------------------
 -- function: site__create_schema                                              --
