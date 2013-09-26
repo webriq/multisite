fb25bf31be203de746791109aaddd38eeecf9c22
David Pozsar <david.pozsar@megaweb.hu>
2013-09-26 15:01:37 +0200

    fix patch

--- module/MultisitePlatform/sql/grid-multisiteplatform/central/data.0-0.1.sql
+++ module/MultisitePlatform/sql/grid-multisiteplatform/central/data.0-0.1.sql
@@ -1,5 +1,16 @@
 -- default values for table: settings
 
-INSERT INTO "_central"."settings" ( "key", "value" )
-                           VALUES ( 'site.createSchema.referenceSchema',
-                                    '_template' );
+DO LANGUAGE plpgsql $$
+BEGIN
+
+    IF NOT EXISTS ( SELECT "value"
+                      FROM "_central"."settings"
+                     WHERE "key" = 'site.createSchema.referenceSchema' ) THEN
+
+        INSERT INTO "_central"."settings" ( "key", "value" )
+                                   VALUES ( 'site.createSchema.referenceSchema',
+                                            '_template' );
+
+    END IF;
+
+END $$;
